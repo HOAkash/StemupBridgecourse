cfacd8a4331dd06e2672a53ee6275eff079889ba
HOAkash <akasha1664@gmail.com>
2025-06-30 22:08:23 +0530

    Create D1_1.java

---
+++ Day1/D1_1.java
@@ -0,0 +1,11 @@
+class D1_1{
+    public static void main(String[]args){
+       String name="akash";
+       int age=21;
+        System.out.println("hello "+name+" you are "+age+" years old " );
+        
+
+    }
+
+    
+}
